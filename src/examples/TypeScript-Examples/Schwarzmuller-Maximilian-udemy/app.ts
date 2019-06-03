5b8737dd4851f2d18cae89095bf84f6665d17b49
Gourav B <pit.wire@pm.me>
2019-06-03 13:07:02 -0400

    tuples: order of type declaration is important

--- src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app.ts
+++ src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app.ts
@@ -3,3 +3,7 @@ let hobbies: any[] = ["Cooking", "Sports"];
 hobbies =[100];
 // setting array type to any overrides
 // implicit type inference by ts
+
+// tuples
+let address: [number, string] = ["Superstreet", 99];
+// order in type declaration is important
