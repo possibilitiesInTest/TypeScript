40dfc6595b1d7a1fc733b51c250e3a6a29ab595e
Gourav B <pit.wire@pm.me>
2019-06-03 13:14:37 -0400

    enum: type restricting variables to vals from predefined set

--- src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app.ts
+++ src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app.ts
@@ -7,3 +7,16 @@ hobbies =[100];
 // tuples
 let address: [number, string] = ["Superstreet", 99];
 // order in type declaration is important
+
+// enum
+// :: type restricting variables to values from
+// predefined set of constants (via Stijin de Witt)
+
+enum Color {
+    Gray, // 0
+    Green = 100, //100
+    Blue // 101
+};
+
+let myColor: Color = Color.Green;
+console.log(myColor);
