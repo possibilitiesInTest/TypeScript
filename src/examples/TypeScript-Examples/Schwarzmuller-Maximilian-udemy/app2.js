dfb7a1749865b5eb6b2c90160342cdca6042d5fd
Gourav Bhattacharyya <pit.wire@pm.me>
2019-06-08 17:39:09 -0400

    TS: using classes to init { }s examples

--- src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app2.js
+++ src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app2.js
@@ -132,4 +132,134 @@ var OnlyOne = /** @class */ (function () {
 }());
 // let wrong = new OnlyOne('The Only One');
 var right = OnlyOne.getInstance();
-console.log(OnlyOne);
+console.log(right.name);
+// right.name = "Something else";
+// readonly prevents overriding
+//////////////////////////////////////
+// Examples
+// Exercise 1 - How was your TypeScript Class?
+var Car = /** @class */ (function () {
+    function Car(name) {
+        this.acceleration = 0;
+        this.name = name;
+    }
+    Car.prototype.honk = function () {
+        console.log("Toooot!");
+    };
+    Car.prototype.accelerate = function (speed) {
+        this.acceleration = this.acceleration + speed;
+    };
+    return Car;
+}());
+var car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(20);
+console.log(car.acceleration);
+// function Car(name) {
+//     this.name = name;
+//     this.acceleration = 0;
+//
+//     this.honk = function() {
+//         console.log("Toooooooooot!");
+//     };
+//
+//     this.accelerate = function(speed) {
+//         this.acceleration = this.acceleration + speed;
+//     }
+// }
+// var car = new Car("BMW");
+// car.honk();
+// console.log(car.acceleration);
+// car.accelerate(10);
+// console.log(car.acceleration);
+////////////////////
+////////////////////
+////////////////////
+// Exercise 2 - Two objects, based on each other ...
+var BaseObject = /** @class */ (function () {
+    function BaseObject() {
+        this.width = 0;
+        this.length = 0;
+    }
+    return BaseObject;
+}());
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Rectangle.prototype.calcSize = function () {
+        return this.width * this.length;
+    };
+    return Rectangle;
+}(BaseObject));
+var rectangle = new Rectangle();
+rectangle.width = 5;
+rectangle.length = 10;
+console.log(rectangle.calcSize());
+// var baseObject = {
+//     width: 0,
+//     length: 0
+// };
+// var rectangle = Object.create(baseObject);
+// rectangle.width = 5;
+// rectangle.length = 2;
+// rectangle.calcSize = function() {
+//     return this.width * this.length;
+// };
+// console.log(rectangle.calcSize());
+////////////////////
+////////////////////
+////////////////////
+// Exercise 3 - Make sure to compile to ES5 (set the target in tsconfig.json)
+var Person2 = /** @class */ (function () {
+    function Person2() {
+        this._firstName = "";
+    }
+    Object.defineProperty(Person2.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._firstName = value;
+            }
+            else {
+                this._firstName = "";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Person2;
+}());
+var person2 = new Person2();
+console.log(person2.firstName);
+person2.firstName = "Ma";
+console.log(person2.firstName);
+person2.firstName = "Maximillian";
+console.log(person2.firstName);
+// var person2 = {
+//     _firstName: ""
+// };
+// Object.defineProperty(person2, "firstName", {
+//     get: function () {
+//         return this._firstName;
+//     },
+//     set: function (value) {
+//         if (value.length > 3) {
+//             this._firstName = value;
+//         }
+//         else {
+//             this._firstName = "";
+//         }
+//     },
+//     enumerable: true,
+//     configurable: true
+// });
+// console.log(person.firstName);
+// person2.firstName = "Ma";
+// console.log(person.firstName);
+// person2.firstName = "Maximilian";
+// console.log(person.firstName);
--- src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app2.ts
+++ src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app2.ts
@@ -107,8 +107,11 @@ console.log(newProject);
 // Private Constructors && Singletons
 class OnlyOne {
     private static instance: OnlyOne;
+    public readonly name: string;
 
-    private constructor(public name: string) {}
+    private constructor(name: string) {
+        this.name = name;
+    }
 
     static getInstance() {
         if(!OnlyOne.instance) {
@@ -120,4 +123,143 @@ class OnlyOne {
 
 // let wrong = new OnlyOne('The Only One');
 let right = OnlyOne.getInstance();
-console.log(OnlyOne);
+console.log(right.name);
+// right.name = "Something else";
+// readonly prevents overriding
+
+
+//////////////////////////////////////
+// Examples
+
+
+// Exercise 1 - How was your TypeScript Class?
+class Car {
+    name: string;
+    acceleration: number = 0;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    honk() {
+        console.log("Toooot!");
+    }
+
+    accelerate(speed: number) {
+        this.acceleration = this.acceleration + speed;
+    }
+}
+
+const car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(20);
+console.log(car.acceleration);
+
+// function Car(name) {
+//     this.name = name;
+//     this.acceleration = 0;
+//
+//     this.honk = function() {
+//         console.log("Toooooooooot!");
+//     };
+//
+//     this.accelerate = function(speed) {
+//         this.acceleration = this.acceleration + speed;
+//     }
+// }
+// var car = new Car("BMW");
+// car.honk();
+// console.log(car.acceleration);
+// car.accelerate(10);
+// console.log(car.acceleration);
+
+
+////////////////////
+////////////////////
+////////////////////
+
+
+// Exercise 2 - Two objects, based on each other ...
+class BaseObject {
+    width = 0
+    length = 0
+}
+
+class Rectangle extends BaseObject {
+    calcSize() {
+        return this.width * this.length;
+    }
+}
+
+const rectangle = new Rectangle();
+rectangle.width = 5;
+rectangle.length = 10;
+console.log(rectangle.calcSize());
+
+// var baseObject = {
+//     width: 0,
+//     length: 0
+// };
+// var rectangle = Object.create(baseObject);
+// rectangle.width = 5;
+// rectangle.length = 2;
+// rectangle.calcSize = function() {
+//     return this.width * this.length;
+// };
+// console.log(rectangle.calcSize());
+
+////////////////////
+////////////////////
+////////////////////
+
+
+// Exercise 3 - Make sure to compile to ES5 (set the target in tsconfig.json)
+class Person2 {
+    private _firstName: string = "";
+
+    get firstName() {
+        return this._firstName;
+    }
+
+    set firstName(value: string) {
+        if (value.length > 3) {
+            this._firstName = value;
+        } else {
+            this._firstName = "";
+        }
+    }
+}
+
+const person2 = new Person2();
+console.log(person2.firstName);
+person2.firstName = "Ma";
+console.log(person2.firstName);
+person2.firstName = "Maximillian";
+console.log(person2.firstName);
+
+
+
+// var person2 = {
+//     _firstName: ""
+// };
+// Object.defineProperty(person2, "firstName", {
+//     get: function () {
+//         return this._firstName;
+//     },
+//     set: function (value) {
+//         if (value.length > 3) {
+//             this._firstName = value;
+//         }
+//         else {
+//             this._firstName = "";
+//         }
+//     },
+//     enumerable: true,
+//     configurable: true
+// });
+// console.log(person.firstName);
+// person2.firstName = "Ma";
+// console.log(person.firstName);
+// person2.firstName = "Maximilian";
+// console.log(person.firstName);
