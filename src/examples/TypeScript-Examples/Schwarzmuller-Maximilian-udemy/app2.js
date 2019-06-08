0a37bb965165fba1fc93eaad6e2c3716eb7b2507
Gourav Bhattacharyya <pit.wire@pm.me>
2019-06-08 15:03:31 -0400

    initing properties on declaration
    creating public, private, protected methods
    accessing private, protected properties through methods

--- src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app2.js
+++ src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app2.js
@@ -3,10 +3,21 @@
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
+        this.age = 27;
         this.name = name;
     }
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+        this.setType("Warm Guy");
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
     return Person;
 }());
 var person = new Person("Max", "max");
 console.log(person);
 console.log(person.username, person.name);
+person.printAge();
+// person.setType("Cool guy");
--- src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app2.ts
+++ src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app2.ts
@@ -4,13 +4,25 @@
 class Person {
     name: string;
     private type: string;
-    protected age: number;
+    protected age: number =27;
 
     constructor(name: string, public username: string) {
         this.name = name;
-    }
+        }
+
+        printAge() {
+            console.log(this.age);
+            this.setType("Warm Guy");
+        }
+
+        private setType(type: string) {
+            this.type = type;
+            console.log(this.type);
+        }
 }
 
 const person = new Person("Max", "max");
 console.log(person);
 console.log(person.username, person.name);
+person.printAge();
+// person.setType("Cool guy");
