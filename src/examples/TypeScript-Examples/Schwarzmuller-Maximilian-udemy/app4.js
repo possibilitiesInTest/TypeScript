57dbd23a2ea51b3b23b8c50eb7df7540f47c35ee
Gourav B <pit.wire@pm.me>
2019-06-11 13:30:09 -0400

    generic types makes code more flexible/manageable at compilation

--- src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app4.js
+++ src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app4.js
@@ -56,3 +56,18 @@ var oldPerson = {
 // console.log(oldPerson);
 ////////////////////////////////////////
 // Generics
+// Simple Generic
+function echo(data) {
+    return data;
+}
+console.log(echo("Max").length);
+console.log(echo(27).length);
+console.log(echo({ name: "Max", age: 27 }));
+// better Generic
+function betterEcho(data) {
+    return data;
+}
+;
+console.log(betterEcho("Max").length);
+console.log(betterEcho(27));
+console.log(betterEcho({ name: "Max", age: 27 }));
--- src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app4.ts
+++ src/examples/TypeScript-Examples/Schwarzmuller-Maximilian-udemy/app4.ts
@@ -86,3 +86,20 @@ const oldPerson: AgedPerson = {
 ////////////////////////////////////////
 // Generics
 
+// Simple Generic
+function echo(data: any) {
+    return data;
+}
+
+console.log(echo("Max").length);
+console.log(echo(27).length);
+console.log(echo({name: "Max", age: 27}));
+
+// better Generic
+function betterEcho<T>(data: T){
+    return data;
+};
+
+console.log(betterEcho("Max").length);
+console.log(betterEcho<number>(27));
+console.log(betterEcho({name: "Max", age: 27}));
