dddd97b999299b98a3ccf1c213ff5a1e7c6f636b
Gourav B <pit.wire@pm.me>
2019-07-20 19:53:06 -0400

    eliminate api surface of google map to restrict functionality:
    CustomMap.ts
         -export CustomMap class
         -init private googleMap property and define its type
         -create constructor, set gM type def from index.ts
         -pass divId as arg to constructor
    
    index.ts
         -import { CustomMap }
         -init new CustomMap()
         -pass map id to CustomMap

--- src/Course-Work/Girder-Stephen-TS/maps/src/index.ts
+++ src/Course-Work/Girder-Stephen-TS/maps/src/index.ts
@@ -1,18 +1,3 @@
-import { User } from './User';
-import { Company } from './Company';
+import { CustomMap } from "./CustomMap";
 
-
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
-
-
-new google.maps.Map(document.getElementById('map'), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+new CustomMap('map');
