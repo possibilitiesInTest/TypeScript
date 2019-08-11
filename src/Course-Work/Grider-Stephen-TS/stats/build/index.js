f119493b11839d1d82622aa32072c66035ed22d8
Gourav B <pit.wire@pm.me>
2019-08-11 09:45:28 -0400

    extract CsvFileReader logic to separate file and import into index, passing in football.csv

--- src/Course-Work/Grider-Stephen-TS/stats/build/index.js
+++ src/Course-Work/Grider-Stephen-TS/stats/build/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('hi!');
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader("football.csv");
+reader.read();
+var MatchResult = {
+    homeWin: "H",
+    awayWin: "A",
+    draw: "D"
+};
+var manUnitedWins = 0;
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === "Man United" && match[5] === MatchResult.homeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResult.awayWin) {
+        manUnitedWins++;
+    }
+}
+console.log("Man United won " + manUnitedWins + " games");
--- src/Course-Work/Grider-Stephen-TS/stats/src/index.ts
+++ src/Course-Work/Grider-Stephen-TS/stats/src/index.ts
@@ -1,15 +1,7 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8"
-  })
-  .split("\n")
-  .map(
-    (row: string): string[] => {
-      return row.split(",");
-    }
-  );
+const reader = new CsvFileReader("football.csv");
+reader.read();
 
 const MatchResult = {
   homeWin: "H",
@@ -19,10 +11,10 @@ const MatchResult = {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === homeWin) {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.homeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === awayWin) {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.awayWin) {
     manUnitedWins++;
   }
 }
